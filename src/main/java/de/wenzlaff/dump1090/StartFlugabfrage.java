9eb7e22eeafc8c12a5fafdbb26c96b03ee9d7998
Thomas Wenzlaff <karotz@wenzlaff.de>
2016-11-23 19:17:06 +0100

    Version wird nun aus der pom.properties gelesen durch neue Klasse Version

--- src/main/java/de/wenzlaff/dump1090/StartFlugabfrage.java
+++ src/main/java/de/wenzlaff/dump1090/StartFlugabfrage.java
@@ -1,5 +1,7 @@
 package de.wenzlaff.dump1090;
 
+import java.io.InputStream;
+import java.util.Properties;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -9,6 +11,7 @@ import org.slf4j.LoggerFactory;
 
 import de.wenzlaff.dump1090.action.PushoverAktion;
 import de.wenzlaff.dump1090.action.TimerAktion;
+import de.wenzlaff.dump1090.action.Version;
 
 /**
  * Startet eine Abfrage.
@@ -20,7 +23,7 @@ public class StartFlugabfrage {
 
 	private static final Logger LOG = LoggerFactory.getLogger(StartFlugabfrage.class);
 
-	private static final String VERSION = "0.0.3";
+	private static final String VERSION = new Version().getVersion();
 
 	/** Plant die Ausführungen. */
 	private ScheduledExecutorService scheduler;
@@ -80,6 +83,40 @@ public class StartFlugabfrage {
 	private void startAnzahlProTagTimer(String ipAdresse, String intervalInMinuten) {
 		LOG.info("Frage nun alle " + intervalInMinuten + " Minuten ohne weitere Ausgaben ab ...");
 		scheduler.scheduleAtFixedRate(new TimerAktion(ipAdresse), 0, Long.parseLong(intervalInMinuten), TimeUnit.MINUTES);
+	}
+
+	private synchronized String getVersion() {
+		String version = null;
+
+		// try to load from maven properties first
+		try {
+			Properties p = new Properties();
+			InputStream is = getClass().getResourceAsStream("/META-INF/maven/de.wenzlaff.dump1090/de/wenzlaff/dump1090/pom.properties");
+			if (is != null) {
+				p.load(is);
+				version = p.getProperty("version", "");
+			}
+		} catch (Exception e) {
+			// ignore
+		}
+
+		// fallback to using Java API
+		if (version == null) {
+			Package aPackage = getClass().getPackage();
+			if (aPackage != null) {
+				version = aPackage.getImplementationVersion();
+				if (version == null) {
+					version = aPackage.getSpecificationVersion();
+				}
+			}
+		}
+
+		if (version == null) {
+			// we could not compute the version so use a blank
+			version = "";
+		}
+
+		return version;
 	}
 
 }
