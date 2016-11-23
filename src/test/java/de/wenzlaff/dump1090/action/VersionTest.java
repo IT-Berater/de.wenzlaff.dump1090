d0f141765f559971750312da56918e95c387914d
Thomas Wenzlaff <karotz@wenzlaff.de>
2016-11-23 20:01:02 +0100

    Assert erg.

--- src/test/java/de/wenzlaff/dump1090/action/VersionTest.java
+++ src/test/java/de/wenzlaff/dump1090/action/VersionTest.java
@@ -1,5 +1,7 @@
 package de.wenzlaff.dump1090.action;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 /**
@@ -14,6 +16,7 @@ public class VersionTest {
 	public void testGetVersion() {
 		Version v = new Version();
 		System.out.println(v.getVersion());
+		assertTrue(v.getVersion().length() >= 0);
 	}
 
 }
