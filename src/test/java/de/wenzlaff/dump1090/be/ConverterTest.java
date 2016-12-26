f6cad99abcc517d435bc80c5a94721085a1558b1
Thomas Wenzlaff <karotz@wenzlaff.de>
2016-12-26 16:12:25 +0100

    Test für Build aus.

--- src/test/java/de/wenzlaff/dump1090/be/ConverterTest.java
+++ src/test/java/de/wenzlaff/dump1090/be/ConverterTest.java
@@ -2,7 +2,12 @@ package de.wenzlaff.dump1090.be;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Properties;
+
+import org.junit.Before;
 import org.junit.Test;
+
+import de.wenzlaff.dump1090.action.SetupReader;
 
 /**
  * Test des Konverters.
@@ -12,14 +17,28 @@ import org.junit.Test;
  */
 public class ConverterTest {
 
+	/** Damit nur lokal getestet wird wenn true. Bei false werden nicht alle Tests ausgeführt. */
+	private boolean lokalerTestmodus;
+
+	@Before
+	public void ini() {
+		Properties p = SetupReader.getProperties();
+		lokalerTestmodus = Boolean.valueOf(p.getProperty("lokaler_testmodus", "false"));
+		System.out.println("Testmodus: " + lokalerTestmodus);
+	}
+
 	@Test
 	public void testGetKmVonKnoten() {
-		assertEquals("2", Converter.getKmVonKnoten("1").toPlainString());
+		if (lokalerTestmodus) {
+			assertEquals("2", Converter.getKmVonKnoten("1").toPlainString());
+		}
 	}
 
 	@Test
 	public void testGetMeterVonFuss() {
-		assertEquals("1", Converter.getMeterVonFuss("0.3048").toPlainString());
+		if (lokalerTestmodus) {
+			assertEquals("1", Converter.getMeterVonFuss("0.3048").toPlainString());
+		}
 	}
 
 }
